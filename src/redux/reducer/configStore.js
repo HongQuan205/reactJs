5045e2369977b0a961d71fabdf023cd2835217bc
Quân Đỗ <hongquan20500@gmail.com>
2021-09-18 14:34:02 +0700

    first commit

---
+++ src/redux/reducer/configStore.js
@@ -0,0 +1,10 @@
+import { combineReducers, createStore } from 'redux';
+
+
+const rootReducer = combineReducers({
+    //reducer tai day
+})
+
+
+ const store = createStore(rootReducer);
+ export default store;
